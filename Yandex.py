41a8aaafc57e46530f29f1b5e12dd96fb1e1eb21
Denis Azarenko <den.azarenko.98@gmail.com>
2020-09-12 03:28:19 +0800

    The tooken

--- Yandex.py
+++ Yandex.py
@@ -1,7 +1,7 @@
 import requests
 import urllib
 
-headers = {'Authorization': 'AgAAAABDmj3oAADLW-_Zo1wKokfgtju9vgR0prQ'}
+headers = {'Authorization': 'мой_токен'}
 response = requests.get("https://cloud-api.yandex.net/v1/disk/resources?path=Ph", headers=headers, params={"path": "C:\\User\denaz\PycharmProjects\\untitled\gif"})
 print(response.text)
 with open('gif/unnamed.jpg', 'rb') as f:
