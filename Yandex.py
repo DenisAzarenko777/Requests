50b39bc4b0d0fd6e8d4721598142e05b907b46b2
Denis Azarenko <den.azarenko.98@gmail.com>
2020-09-20 19:10:16 +0800

    commit

--- Yandex.py
+++ Yandex.py
@@ -1,9 +1,7 @@
 import requests
 import urllib
-
-headers = {'Authorization': 'мой_токен'}
-response = requests.get("https://cloud-api.yandex.net/v1/disk/resources?path=Ph", headers=headers, params={"path": "C:\\User\denaz\PycharmProjects\\untitled\gif"})
-print(response.text)
-with open('gif/unnamed.jpg', 'rb') as f:
-    resp = requests.put(response.url, files = {"file":f})
-#https://cloud-api.yandex.net/v1/disk/resources?path=Ph
+headers = {'Authorization': "<Мой токен>"}
+response = requests.get("https://cloud-api.yandex.net/v1/disk/resources?path=Ph", headers=headers, params={"path": "C:\\User\denaz\PycharmProjects\\untitled"})
+with open('ph.jpg', 'rb') as f:
+    resp = requests.put(response.url, files = {"ph.jpg":f})
+print(response.url)
