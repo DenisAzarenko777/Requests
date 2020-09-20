01215ed8457e936237914363f2994c6f36313f3d
Denis Azarenko <den.azarenko.98@gmail.com>
2020-09-20 15:47:09 +0800

    up to date

--- requestss.py
+++ requestss.py
@@ -1,20 +1,15 @@
 import requests
 
 class Super_hiro:
-
-    def list_with_object(self):
+    def list_with_object2(self,super_list):
         global_list = list()
-        for i in range(0,3,1):
-            if i == 0:
-                name = "Hulk"
-            if i == 1:
-                name = "Thanos"
-            if i == 2:
-                name = "Captain America"
-            response = requests.get(f"https://superheroapi.com/api/2619421814940190/search/{name}")
+        self.super_list = super_list
+        for element in super_list:
+            response = requests.get(f"https://superheroapi.com/api/2619421814940190/search/{element}")
             print(response.json())
             global_list.append(response.json())
         return global_list
+
 
     def super_intelligence(self, gl):
         global_list = gl
@@ -26,8 +21,12 @@ class Super_hiro:
                 print(elem["powerstats"]["intelligence"])
                 intelligence = elem["powerstats"]["intelligence"]
                 super_dict[elem["name"]] = f"intelligence: {intelligence}"
-
         return super_dict
 
+hero_list_data = ['Hulk', 'Captain', 'America', 'Thanos']
 Superhiro = Super_hiro()
-print(Superhiro.super_intelligence(Superhiro.list_with_object()))
+print(Superhiro.super_intelligence(Superhiro.list_with_object2(hero_list_data)))
+
+
+
+
